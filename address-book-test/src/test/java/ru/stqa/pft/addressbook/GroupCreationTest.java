7b1aee752c584293df5918369530f4d90cddfb1a
test <natitshow@gmail.com>
2020-02-15 23:14:34 +0300

    changed annotation Before and Test

--- address-book-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -14,10 +14,6 @@ public class GroupCreationTest {
         System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver_win32\\chromedriver.exe");
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void testGroupCreation() throws Exception {
         wd.get("http://localhost/addressbook/group.php?selected%5B%5D=1&delete=Delete+group%28s%29");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -26,6 +22,10 @@ public class GroupCreationTest {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testGroupCreation() throws Exception {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
