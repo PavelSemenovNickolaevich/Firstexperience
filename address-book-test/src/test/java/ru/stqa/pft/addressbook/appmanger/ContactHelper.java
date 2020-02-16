2879ff5b84bf0e9a2cbf717165678518973ef4f5
test <natitshow@gmail.com>
2020-02-16 15:44:51 +0300

    1)Added net test "ContactCreationTest" Refactor ContactCreationTest and added auxiliary method ContactData 2)Added TestBase Class 4)Added AppManager and refactoring 5)Added helper

---
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/appmanger/ContactHelper.java
@@ -0,0 +1,45 @@
+package ru.stqa.pft.addressbook.appmanger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactHelper {
+    private WebDriver wd;
+
+    public ContactHelper(WebDriver wd) {
+        this.wd = wd;
+    }
+
+    public void logoutContact() {
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    public void fillContactInfo(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlename());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAdress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+    }
+
+    public void returnHomeContact() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    public void submitContact() {
+        wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+}
