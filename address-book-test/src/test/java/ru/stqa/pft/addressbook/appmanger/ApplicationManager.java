fb843ca0d36b9383aa98515505c83f1a725f1138
test <natitshow@gmail.com>
2020-02-21 11:50:20 +0300

    Joined Firefox, Chrome. Made if - construction

--- address-book-test/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/appmanger/ApplicationManager.java
@@ -2,17 +2,28 @@ package ru.stqa.pft.addressbook.appmanger;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final String browser;
     private WebDriver wd;
     private NavigationHelper navigationHelper;
     private ContactHelper contactHelper;
     private GroupHelper groupHelper;
 
+    public ApplicationManager (String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(BrowserType.FIREFOX)){
+            wd = new FirefoxDriver();
+        } else {
+            wd = new ChromeDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/group.php?selected%5B%5D=1&delete=Delete+group%28s%29");
         groupHelper = new GroupHelper(wd);
--- address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanger.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager applicationManager = new ApplicationManager();
+    protected final ApplicationManager applicationManager = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Windows\\System32\\chromedriver8.0_win32\\chromedriver.exe");
         applicationManager.init();
     }
 
