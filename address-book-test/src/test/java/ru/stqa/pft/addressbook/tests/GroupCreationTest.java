941da5cdd8cdfb5190318b33d71773e1d190f434
pavel <natitshow@gmail.com>
2021-04-07 21:37:34 +0300

    added Lection 5.8. hash checks

--- address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -15,18 +15,26 @@ public class GroupCreationTest extends TestBase {
         Groups before = app.group().all();
         GroupData group = new GroupData().withName("test2").withHeader("213").withFooter("4r32r432");
         app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
-        //Assert.assertEquals(after.size(), before.size() + 1);
-        assertThat(after.size(), equalTo(before.size() + 1));
         //  group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-        group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-        before.add(group);
+ //       before.add(group);
 //        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
 //        before.sort(byId);
 //        after.sort(byId);
         //Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
         //  Assert.assertEquals(before, after);
-        assertThat(after, equalTo(before.withAdded(group)));
+        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
+    @Test
+    public void testBadGroupCreation() throws Exception {
+        app.goTo().groupPage();
+        Groups before = app.group().all();
+        GroupData group = new GroupData().withName("test2").withHeader("213").withFooter("4r32r432");
+        app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size()));
+        Groups after = app.group().all();
+        assertThat(after, equalTo(before));
+    }
 }
--- address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationUpdateTest.java
+++ address-book-test/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationUpdateTest.java
@@ -8,6 +8,9 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GroupModificationUpdateTest extends TestBase {
 
     @BeforeMethod
@@ -25,9 +28,8 @@ public class GroupModificationUpdateTest extends TestBase {
         GroupData group = new GroupData().withId(modifyGroup.getId())
                 .withName("test1").withHeader("test2").withFooter("test3");
         app.group().modify(group);
+        assertThat(app.group().count(), equalTo(before.size()));
         Groups after = app.group().all();
-        Assert.assertEquals(after.size(), before.size());
-
         before.remove(modifyGroup);
         before.add(group);
 //        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
